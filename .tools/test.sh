23ab973276c133eb01c00812d88dfa84416f7c18
Oleh Prypin <oleh@pryp.in>
2024-04-27 12:13:11 +0200

    Fix CI

--- .tools/test.sh
+++ .tools/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -e
+set -e -x
 
 cd "$(dirname "$0")/.."
 
-mkdocs build -f examples/good/mkdocs.yml -q --strict
+mkdocs build -f examples/good/mkdocs.yml --strict
 mkdocs --version | grep -E '\b1\.[01]\.' && exit 0 || true
-mkdocs build -f examples/bad/mkdocs.yml -q --strict
-! env LINT=true mkdocs build -f examples/bad/mkdocs.yml -q --strict >/dev/null 2>/dev/null
+mkdocs build -f examples/bad/mkdocs.yml --strict
+! env LINT=true mkdocs build -f examples/bad/mkdocs.yml --strict
